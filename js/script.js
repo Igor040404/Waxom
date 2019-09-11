eac35cf14e9f93833aa02b5f751ba2d954d8cc54
TikTak04 <orishchak.igor@meta.ua>
2019-09-12 00:12:27 +0300

    First version of site.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+(function($) {
+    // all JS code here
+    $('.slider').slick({
+        dots: true,
+        nextArrow: '<div class="slick-arrow slick-next"><img  src="img/back/right.svg" alt""></div>',
+        prevArrow: '<div class="slick-arrow slick-prev"><img src="img/back/left.svg" alt""></div>',
+    });
+})(jQuery);
